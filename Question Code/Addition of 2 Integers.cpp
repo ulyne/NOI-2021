47e4ca308d347242a92f5ee98509decf483cd2aa
ulyne <80171064+ulyne@users.noreply.github.com>
2021-11-12 16:45:19 +0800

    Create Addition of 2 Integers.cpp

---
+++ Question Code/Addition of 2 Integers.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  long long int a,b,sum;
+  cin >> a >> b;
+  sum = a + b;
+  cout << sum;
+
+  return 0;
+}

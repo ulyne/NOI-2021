7fa9975c949e36b8397706b8aa7c916a1942b0a4
ulyne <80171064+ulyne@users.noreply.github.com>
2021-11-13 21:13:20 +0800

    Create 90DTriangle.cpp

---
+++ Question Code/90DTriangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  long long n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j <= i; j++){
+      cout << "*";
+    } 
+    cout << "\n";
+  }
+  return 0;
+} 
